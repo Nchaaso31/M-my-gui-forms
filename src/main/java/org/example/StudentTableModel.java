e287b831c1fb6612d6bd7f110176983c1e851ebb
Nchaaso31 <j.angelnchaso@gmail.com>
2022-10-28 11:06:20 +0200

    MyTableForm ejercicio prueba

---
+++ src/main/java/org/example/StudentTableModel.java
@@ -0,0 +1,62 @@
+package org.example;
+
+import javax.swing.table.AbstractTableModel;
+import java.util.List;
+
+public class StudentTableModel extends AbstractTableModel {
+    private final static int NAME_COLUMN_INDEX = 1;
+    private final static int SURNAME_COLUMN_INDEX = 2;
+    private final static int NIF_COLUMN_INDEX = 0;
+    private final static int AGE_COLUMN_INDEX = 3;
+
+    private List<Student> students;
+
+    public StudentTableModel(List<Student> students) {
+        super();
+        this.students = students;
+    }
+
+    @Override
+    public int getRowCount() {
+        return students.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return 4;
+    }
+
+    @Override
+    public String getColumnName(int column) {
+        switch (column) {
+            case NAME_COLUMN_INDEX:
+                return "Nombre";
+            case SURNAME_COLUMN_INDEX:
+                return "Apellidos";
+            case NIF_COLUMN_INDEX:
+                return "NIF";
+            case AGE_COLUMN_INDEX:
+                return "Edad";
+            default:
+                throw new RuntimeException("No existe la columna " + column);
+        }
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        Student student = students.get(rowIndex);
+        switch (columnIndex) {
+            case NAME_COLUMN_INDEX:
+                return student.getName();
+            case SURNAME_COLUMN_INDEX:
+                return student.getSurname();
+            case NIF_COLUMN_INDEX:
+                return student.getNif();
+            case AGE_COLUMN_INDEX:
+                return student.getAge();
+            default:
+                throw new RuntimeException("No existe la columna " + columnIndex);
+        }
+
+    }
+}
---
+++ src/main/java/org/example/myTableForm.java
@@ -0,0 +1,55 @@
+package org.example;
+
+import javax.swing.*;
+import java.util.List;
+
+public class myTableForm {
+    private JPanel mainPanel;
+    private JTable myTableForm;
+    public myTableForm() {
+        StudentTableModel model = new StudentTableModel(
+                List.of(
+                        new Student("10000000X","Peppa", "Pig",    4),
+                        new Student("10000001X","George", "Pig",    2),
+                        new Student("10000002X","Bob", "Esponja",    3),
+                        new Student("10000003X","P", "Pocoyo",    6),
+                        new Student("10000000X","Peppa", "Pig",    4),
+                        new Student("10000001X","George", "Pig",    2),
+                        new Student("10000002X","Bob", "Esponja",    3),
+                        new Student("10000003X","P", "Pocoyo",    6),
+                        new Student("10000000X","Peppa", "Pig",    4),
+                        new Student("10000001X","George", "Pig",    2),
+                        new Student("10000002X","Bob", "Esponja",    3),
+                        new Student("10000003X","P", "Pocoyo",    6),
+                        new Student("10000000X","Peppa", "Pig",    4),
+                        new Student("10000001X","George", "Pig",    2),
+                        new Student("10000002X","Bob", "Esponja",    3),
+                        new Student("10000003X","P", "Pocoyo",    6),
+                        new Student("10000000X","Peppa", "Pig",    4),
+                        new Student("10000001X","George", "Pig",    2),
+                        new Student("10000002X","Bob", "Esponja",    3),
+                        new Student("10000003X","P", "Pocoyo",    6),new Student("10000000X","Peppa", "Pig",    4),
+                        new Student("10000001X","George", "Pig",    2),
+                        new Student("10000002X","Bob", "Esponja",    3),
+                        new Student("10000003X","P", "Pocoyo",    6),
+                        new Student("10000000X","Peppa", "Pig",    4),
+                        new Student("10000001X","George", "Pig",    2),
+                        new Student("10000002X","Bob", "Esponja",    3),
+                        new Student("10000003X","P", "Pocoyo",    6)
+                )
+        );
+        myTableForm.setModel(model);
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        var myForm = new myTableForm();
+
+        frame.setContentPane(myForm.mainPanel);
+
+        frame.setVisible(true);
+        frame.setSize(300,200);
+        frame.revalidate();
+        frame.repaint();
+    }
+}
